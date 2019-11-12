ed7ce60caa87278d08c6d56d58f397331cbac299
teddy <teddymczieuwa@gmail.com>
2019-11-12 07:01:06 +0100

    Drop database implemented successfully

---
+++ checkdb.js
@@ -0,0 +1,20 @@
+const db = require('./db')('example1') //('example')
+
+db.dropDatabase(db, (err) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('Database was successfully dropped')
+    }
+})
+
+// db.insertRecord({name: 'Sandra', title:'Mrs'})
+
+
+
+
+// db.createDatabase('example1',[], (err) => {
+//     if(!err) {
+//         console.log('database created')
+//     }
+// })
---
+++ db.js
@@ -0,0 +1,70 @@
+// require modules
+const fs = require('fs')
+const path = require('path')
+
+const db = (model) => {
+    console.log(__dirname)
+
+    // base directory for the database store
+    const baseDir = path.join(__dirname, './.local')
+
+    if (model) {
+        // get the file path
+        var filename = path.join(__dirname, `./.local/${model}.json`)
+
+        // require the file from the file path
+        var records = require(filename)
+    }
+
+    const dropDatabase = (callback) => {
+        console.log('baseDir',baseDir)
+        console.log('file:', model)
+        fs.unlink(`${baseDir}/${model}.json`, (err) => {
+            if(!err) {
+                callback(false)
+            } else {
+                console.log(err)
+                callback('Error deleting file', err)
+            }
+        })
+    }
+    
+
+    const createDatabase = ( file, data, callback) => {
+        //if(!file) return callback('Please select document to add data')
+        fs.open(`${baseDir}/${file}.json`, 'wx', (err, fd) => {
+            if(!err && fd) {
+                // stringify the data
+                const stringData = JSON.stringify(data)
+
+                fs.writeFile(fd, stringData, (err) => {
+                    if(!err) {
+                        // close the file
+                        fs.close(fd,(err) => {
+                            if(!err) {
+                                callback(false)
+                            } else {
+                                callback('Error closing file')
+                            }
+                        })
+                    } else {
+                        callback('Error writing to new File')
+                    }
+                })
+
+            } else {
+                //console.log(err)
+                callback('Could not create data, file may already exist')
+            }
+        })
+    }
+
+    return {
+        records,
+        createDatabase,
+        dropDatabase
+    }
+   
+}
+
+module.exports = db
